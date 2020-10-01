a00b4e95af63ab6cf94aa29c259ffb66148eaa71
KirillRo1606 <67650977+KirillRo1606@users.noreply.github.com>
2020-10-02 00:11:46 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length){
+    return 0;
+  }
+
+  let min = array[0];
+  let i = 1;
+  while (i < array.length) {
+    if (min > array[i]) {
+      min = array[i];
+    }
+    i++;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length){
+    return 0;
+  }
+
+  let max = array[0];
+  let i = 1;
+  while (i < array.length) {
+    if (max < array[i]) {
+      max = array[i];
+    }
+    i++;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length){
+    return 0;
+  }
+
+  let sum = 0;
+  let i = 0;
+  while (i < array.length) {
+    sum = sum + array[i];
+    i++;
+  }
+  return sum / array.length;
 }
